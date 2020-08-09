421e3b72ad5524a4008f7e5a6324751b1083eecc
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-08-09 16:11:24 +0100

    added table body with emplyee's details

--- src/components/EmployeesTable.js
+++ src/components/EmployeesTable.js
@@ -12,6 +12,19 @@ function EmployeesTable () {
           <th scope="col">DOB</th>
         </tr>
       </thead>
+      <tbody>
+				{employees.map((employee) => (
+					<Employee
+						id={employee.id}
+						key={employee.id}
+						name={employee.name}
+						position={employee.position}
+						email={employee.email}
+						dob={employee.dob}
+					/>
+				))}
+			</tbody>
+
     </table>
   )
 }
